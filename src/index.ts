36a74cba7a15253cd63ff8a3f78d95a06b0cb5e9
leimao.ding <leimao.ding@beibei.com>
2019-06-18 19:58:53 +0800

    准备开写Promise.all

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {noop} from './utils';
+import {noop ,isObject ,isDef , isFunc} from './utils';
 
 enum pStatus  {
     pending = 'pending',
@@ -6,7 +6,12 @@ enum pStatus  {
     rejected = 'rejected'
 }
 
-export default function Promise2(cb:Function) {
+export default function Promise2(cb:any) {
+
+    if( !isFunc(cb) ){
+        throw 'Promise2 传递的参数不为functon！！！';
+    }
+
     this.status = pStatus.pending;
 
     this.resovlecbs = [];
@@ -26,10 +31,18 @@ export default function Promise2(cb:Function) {
 
     const reject = (error:Error)=>{
         this.error = error;
-        this.rejectcbs.forEach((item:Function)=>{
-            item(error);
-        })
-        this.status = pStatus.rejected;
+
+        setTimeout(()=>{ // why
+            this.status = pStatus.rejected;
+            if(this.rejectcbs.length ===0){
+                throw this.error;
+            }  else {
+                this.rejectcbs.forEach((item:Function)=>{
+                    item(error);
+                })
+            }
+        },0)
+       // if(this.rejectcbs.length === 0 ) throw error;
     } 
 
     try {
@@ -56,9 +69,10 @@ Promise2.prototype.then = function (resolvecb:Function=noop,rejectcb:Function=no
         } else if(scope.status===pStatus.fulled) {
             let res = resolvecb(scope.value);
             resolve(res);
-        } else { // reject
+        } else { // rejectd
             let res = rejectcb(scope.error);
             reject(res);
+
         }
     });
 }
@@ -66,3 +80,45 @@ Promise2.prototype.then = function (resolvecb:Function=noop,rejectcb:Function=no
 Promise2.prototype.catch = function(catchcb:Function) {
     return this.then(null, catchcb);
 }
+
+// hack  但是不对
+Promise2.prototype.finally = function (callback) {
+   return this.then((value)=>{
+        callback();
+        return value;
+   },callback);
+}
+
+Promise2.resolve = function(handler){
+    if(  isObject(handler)  && 'constructor' in handler && handler.constructor=== this) { // handler 是 Promise2
+        return handler;
+    } else if (isObject(handler) && 'then' in handler){ // thenable
+        return new this(handler.then);
+    } 
+    //else if( isDef(handler)  && (  !isObject(handler) || !('then' in handler)) ) { // 不为then()
+    //     return new this(function(resolve){
+    //         resolve(handler);
+    //     })
+    // }
+    else { // unDef
+        return new this(function(resolve){
+            resolve(handler);
+        })
+    }   
+}
+
+Promise2.reject = function() {
+    const args = Array.prototype.slice.call(arguments);
+    return new this((resolve, reject) => reject(args.shift()));
+}
+
+Promise2.all = function() {}
+
+
+// Promise2.prototype.finally = function (callback) {
+//     let Constructor = this.constructor;
+//     return this.then(
+//       value  => Constructor.resolve(callback()).then(() => value),
+//       reason => Constructor.resolve(callback()).then(() => { throw reason })
+//     );
+// };
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,16 @@
 export function noop (a?: any, b?: any, c?: any) {}
+
+export function isUndef (val) {
+    return val===null || val===undefined;
+}
+
+export function isDef (val) {
+    return val!==null && val!==undefined;
+}
+
+export function isObject(val) {
+  return  Object.prototype.toString.call(val) === "[object Object]";
+}
+export function isFunc(val) {
+    return  Object.prototype.toString.call(val) === "[object Function]";
+  }
